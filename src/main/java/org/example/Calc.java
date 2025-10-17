6fb557ef1e86f484365a19b55b278047acb4f3fe
zhangfw1 <79648989@qq.com>
2025-10-17 22:35:03 +0800

    Implement Calc with add and subtract methods

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,11 @@
 package org.example;
 
 public class Calc {
-
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
